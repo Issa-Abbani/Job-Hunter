dcdd8fa312afc587bb23377a5c0799ddd2165bd1
Issa-Abbani <issaabbani04@gmail.com>
2026-09-08 19:36:50 +0300

    Created proxy.ts to protect my protected routes and prevent users from entering to the auth pages if they are signed in. Also created seeding Job Hunt Dashboard

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,7 +3,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-
+import { initializeUserBoard } from "../init-user-board";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 
@@ -13,25 +13,38 @@ export const auth = betterAuth({
   database: mongodbAdapter(db, {
     client,
   }),
-  emailAndPassword:{
-    enabled: true
-  }
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          if (user.id) {
+            await initializeUserBoard(user.id);
+          }
+        },
+      },
+    },
+  },
 });
 
-export async function getSession(){
+export async function getSession() {
   const result = await auth.api.getSession({
     headers: await headers(),
-  })
+  });
 
   return result;
 }
 
-export async function signOut(){
+export async function signOut() {
   const result = await auth.api.signOut({
     headers: await headers(),
-  })
+  });
 
-  if(result.success){
+  if (result.success) {
     redirect("/sign-in");
   }
 }
